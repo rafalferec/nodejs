b7156bb8cf0a9d8036b2eaa5a05adeec97f5df4a
Rafal Ferec <rafal.ferec@kamikaze.digital>
2019-01-28 09:20:17 +0100

    testowy nodejs 2

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const https = require('https'),
 fs = require("fs")
 express = require('express');
 
-const options = {
-    key: fs.readFileSync("/etc/httpd/conf/ssl/fbkamikaze/fb.kamikaze.com.pl.key"),
-    cert: fs.readFileSync("/etc/httpd/conf/ssl/fbkamikaze/fb.kamikaze.com.pl.crt")
-};
+
 const port = 5000;
 
 
@@ -17,13 +14,13 @@ app.use((req, res) => {
   res.end("hello world\n");
 });
 
-app.get('/webhook', (req, res) => {
-    if (req.query['hub.verify_token'] === '111122223333') {
-      res.send(req.query['hub.challenge']);
-    }
-  });
-// app.listen(5001);
+// app.get('/webhook', (req, res) => {
+//     if (req.query['hub.verify_token'] === '111122223333') {
+//       res.send(req.query['hub.challenge']);
+//     }
+//   });
+app.listen(5000);
 
-https.createServer(options, app).listen(5000);
+// http.createServer(options, app).listen(5000);
 // fkill -f :8080
 // Aby zainstalować polecenie użycia polecenia fkill: npm i -g fkill
